7022728a565f8387fc3f384a7ff6654a91263293
Dario Arrieta <arrieta.dario@hotmail.com>
2020-08-17 18:16:07 -0500

    vista items ok con datos traidos en la misma vista y no en general.

--- src/views/Items.vue
+++ src/views/Items.vue
@@ -1,22 +1,53 @@
 <template>
-  <div>
-    {{firebaseData}}
+  <div class="h-screen flex flex-col">
+    <!-- HEADER -->
+    <div class="h-12 bg-indigo-700 text-gray-100 flex justify-center">
+        <div class="container h-full flex justify-around items-center max-w-3xl">
+            <router-link to='/'>
+                <i class="material-icons" title="Atrás">keyboard_backspace</i>
+            </router-link>
+            <span>ITEMS</span>
+            <i class="material-icons" title="Buscar">search</i>
+        </div>
+    </div>
+    <!-- MAIN -->
+    <div class="h-full overflow-x-hidden overflow-y-scroll flex justify-center">
+        <div class="container max-w-3xl">
+            <div class="w-full h-12 bg-white border p-1 flex flex-col rounded"
+                v-for="i in items"
+                :key="i.id">
+                    <div class="text-lg text-left cursor-pointer hover:text-blue-700">
+                        {{i.nombre}}    
+                    </div>
+                    <div class="text-sm text-right text-gray-500 flex justify-between">
+                        <span>Cod:{{i.cod}}</span>
+                        <span>{{i.familia}} - {{i.tipo}} Stk:{{i.almacen}}</span> 
+                    </div>
+            </div>
+            
+        </div>
+    </div>
+    <!-- FOOTER -->
+    <PxFooter/>
   </div>
 </template>
 
 <script>
 import {db} from '@/firebase'
+import PxFooter from '@/components/PxFooter'
   export default {
     name: 'Items',
+    components:{
+        PxFooter,
+    },
     data(){
       return{
-        firebaseData:null,
+        items:null,
       }
     },
     firestore() {
-      console.log('se crea el componente')
       return{
-        firebaseData: db.collection('items')
+        items: db.collection('items')
       }
     },
   }
