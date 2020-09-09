48d74bf2b76a461d33e6516c46c823238c928bd8
Carlos Riquelme <criquelme2@gmail.com>
2020-09-09 16:29:46 -0300

    Primer commit rama development

--- src/router.js
+++ src/router.js
@@ -1,33 +1,38 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Items from '@/views/Items'
-import ItemDetalle from '@/views/ItemDetalle'
-import ItemNuevo from '@/views/ItemNuevo'
+import Vue from "vue";
+import Router from "vue-router";
+import Items from "@/views/Items";
+import ItemDetalle from "@/views/ItemDetalle";
+import ItemNuevo from "@/views/ItemNuevo";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-    mode:'history',
-    routes:[
-        {
-            path:'/items',
-            name:'Items',
-            component:Items
-        },
-        {
-            path:'/items/:cod',
-            name:'ItemDetalle',
-            component:ItemDetalle
-        },
-        {
-            path:'/itemnuevo',
-            name:'ItemNuevo',
-            component:ItemNuevo
-        },
-        {
-            path:'*',
-            name:'Error',
-            component:Error
-        }
-    ]
-})
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "Items",
+      component: Items,
+    },
+    {
+      path: "/items",
+      name: "Items",
+      component: Items,
+    },
+    {
+      path: "/items/:cod",
+      name: "ItemDetalle",
+      component: ItemDetalle,
+    },
+    {
+      path: "/itemnuevo",
+      name: "ItemNuevo",
+      component: ItemNuevo,
+    },
+    {
+      path: "*",
+      name: "Error",
+      component: Error,
+    },
+  ],
+});
